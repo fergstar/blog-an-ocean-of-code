41c532ba9ed5e334853bae4bc15d152f1a4bcf88
Guy Fergusson <fergs@iinet.net.au>
2018-07-08 20:56:08 +0800

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+  <div>
+    <h1>About</h1>
+    <p>this is the about about page</p>
+    <Link to="/">back home</Link>
+  </div>
+);
+

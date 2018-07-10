2ccb74e083110c1fdfa6a491977c0c2fd5a49347
Guy Fergusson <fergs@iinet.net.au>
2018-07-11 03:03:44 +0800

    add tags. not working

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,12 @@
 const path = require(`path`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
+ const _ = require(`lodash`);
+
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode })
     createNodeField({
       node,
       name: `slug`,
@@ -15,6 +17,10 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
+
+  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`);
+  const tagTemplate = path.resolve(`./src/templates/tags.js`);
+
   return new Promise((resolve, reject) => {
     graphql(`
       {
@@ -24,22 +30,56 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
               fields {
                 slug
               }
+              frontmatter {
+                title
+                path
+                tags
+              }
             }
           }
         }
       }
-    `
-).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  `).then(result => {
+    if (result.errors) {
+      console.log(result.errors);
+      return Promise.reject(result.errors);
+    }
+
+    const posts = result.data.allMarkdownRemark.edges;
+    
+    // Create post detail pages
+    posts.forEach(({ node }) => {
         createPage({
           path: node.fields.slug,
-          component: path.resolve(`./src/templates/blog-post.js`),
+          component: blogPostTemplate,
           context: {
             // Data passed to context is available in page queries as GraphQL variables.
             slug: node.fields.slug,
           },
-        })
-      })
+        });
+    });
+
+    // Tage pages:
+    let tags = [];
+    // Iterate through each post, putting all found tags into 'tags'
+    _.each(posts, edge => {
+      if (_.get(edge, "node.formatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags);
+      }
+    });
+    // eliminate duplicate tags
+    tags = _.uniq(tags);
+
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
+      });
+    });
+
       resolve()
     })
   })
